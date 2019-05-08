17049c29c0aba242a1832d6d406d65a3b192d085
Santiago Herrero <santi@edpuzzle.com>
2019-05-08 10:23:13 +0200

    Add test configuration updates

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -219,6 +219,7 @@ module.exports = function(
 
   console.log();
   console.log(`Success! Created ${appName} at ${appPath}`);
+  console.log(chalk.red('WELCOME TO EDPUZZLE'));
   console.log('Inside that directory, you can run several commands:');
   console.log();
   console.log(chalk.cyan(`  ${displayedCommand} start`));
